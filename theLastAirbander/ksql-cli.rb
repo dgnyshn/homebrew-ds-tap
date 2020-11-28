94db4a4f84149896099ce3fbcf7cccb9d14e531f
theLastAirbander <doganaysahin40@gmail.com>
2020-11-28 22:30:26 +0300

    Update ksql-cli.rb

--- theLastAirbander/ksql-cli.rb
+++ theLastAirbander/ksql-cli.rb
@@ -1,17 +1,29 @@
+require "language/go"
+
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class KsqlCli < Formula
-  desc "A basic interactive prompt for ksql"
+class DockerShell < Formula
+  desc "A simple interactive prompt for ksql"
   homepage "https://github.com/theLastAirbander/ksql-cli"
   url "https://github.com/theLastAirbander/ksql-cli/archive/v.0.0.1.tar.gz"
   sha256 "2b22fe00755b876269c2d5d51ac4ee0c252e1a76289c55db7ded3b556746d309"
+  head "https://github.com/theLastAirbander/ksql-cli"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "ksql-cli"
+
+    ENV["GOPATH"] = buildpath
+    bin_path = buildpath/"src/github.com/theLastAirbander/ksql-cli"
+    bin_path.install Dir["*"]
+
+    cd bin_path do
+      system "go", "build", "-o", bin/"ksql-cli", "."
+    end
   end
 
   test do
-    system "#{bin}/ksql-cli", "--help"
+    system "true"
   end
 end
